e787d796a8568b117c58f4c1eb4724ee1f5fb624
satyasivaganesh <84192998+satyasivaganesh@users.noreply.github.com>
2021-11-03 14:23:05 +0530

    Create middle node of Linkedlist.cpp

---
+++ middle node of Linkedlist.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *s,*f;
+        s=head;
+        f=head;
+        while(f &&f->next)
+            {
+            s=s->next;
+            f=f->next->next;
+            }
+        return s;
+        
+    }
+};
